5c58fdae26aadbb54c826df21432b504cbd3ea2d
Préts Barnabás <prets.barnabas@students.jedlik.eu>
2022-03-31 12:06:22 +0200

    javas fix

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ function Valtozo(artist){
     var valtozoMain = document.getElementById("valtozo-main");
     var valtozoKep = document.getElementById("valtozo-kep");
     var valtozoSzoveg = document.getElementById("valtozo-szoveg");
-    valtozoMain.style.height = "500px";
-    valtozoKep.style.minHeight = "500px";
-    // valtozoKep.style.backgroundImage = "url(assets/misi.jpg)"
-    valtozoSzoveg.style.height = "500px";
-    valtozoSzoveg.style.width = "500px";
-    valtozoKep.innerHTML += "<img src='assets/misi.jpg'>";   
-    valtozoSzoveg.innerHTML += "<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, itaque expedita. Similique officia, beatae soluta doloremque optio doloribus alias rerum ipsam officiis. Temporibus, voluptate ipsam asperiores cum magni exercitationem. Quasi?</p>";
-    onScreen = true;
+    if (!onScreen){
+        valtozoKep.style.minHeight = "500px";
+        valtozoSzoveg.style.height = "500px";
+        valtozoSzoveg.style.width = "500px";
+        onScreen = true;
+    }
+    valtozoKep.innerHTML = "<img src='assets/misi.jpg'>";   
+    valtozoSzoveg.innerHTML = "<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, itaque expedita. Similique officia, beatae soluta doloremque optio doloribus alias rerum ipsam officiis. Temporibus, voluptate ipsam asperiores cum magni exercitationem. Quasi?</p>";
+    return onScreen;
 }
